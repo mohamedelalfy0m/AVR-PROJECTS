b39a7560d367f5c24a6ae905a913a08005239114
Mohamed Elalfy <131621928+mohamedelalfy0m@users.noreply.github.com>
2023-08-16 17:39:19 +0300

    Add files via upload

---
+++ 7segment-4bit/7segment 4bit/7segment 4bit/main.c
@@ -0,0 +1,28 @@
+/*
+ * 7segment 4bit.c
+ *
+ * Created: 8/16/2023 4:49:55 PM
+ * Author : user
+ */ 
+
+#include <avr/io.h>
+#include <util/delay.h>
+#define LED_direction DDRA		/* define LED Direction */
+#define LED_PORT PORTA			/* define LED PORT */
+
+int main(void)
+{
+	LED_direction |= 0xff;		/* define LED port direction is output */
+	LED_PORT = 0xff;
+	char array[]={0,1,2,3,4,5,6,7,8,9};
+	/* write BCD value for CA display from 0 to 9 */
+	while(1)
+	{
+		for(int i=0;i<10;i++)
+		{
+			LED_PORT = array[i];/* write data on to the LED port */
+			_delay_ms(1000); /* wait for 1 second */
+		}
+	}
+}
+
